63bfe251f1e90d431dfba8169de91ac5f2e95a85
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-16 23:14:18 +0600

    chore: update ESLint configuration and Next.js settings; add middleware file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
