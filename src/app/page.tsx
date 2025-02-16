a715cfd0caf4fc188dd4d6d4fff8b0f49cc342ea
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-16 23:00:32 +0600

    implement Home, Error, Loading, and NotFound components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
-export default function Home() {
-  return (
-    <div className="text-slate-800">
-      <h3>Wellcome</h3>
-    </div>
-  );
-}
+
+const HomePage = () => {
+
+    return (
+        <div>
+            Sakhawat
+        </div>
+    );
+};
+
+export default HomePage;
