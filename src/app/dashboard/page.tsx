014bddd0705bf7e6ce89ed378c22d5d9880804f4
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-28 01:57:57 +0600

    refactor: move DashboardClient component to a new file and update imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import DashboardClient from "@/components/Dashboard/Dashboard";
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 
@@ -9,7 +8,6 @@ const DashboardPage = async () => {
         return <div className="text-center mt-10 text-red-500">Unauthorized Access</div>;
     }
 
-    // return <DashboardClient user={session.user} />;
 };
 
 export default DashboardPage;
